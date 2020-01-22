6a5aca1b37ddba216ffc74aacbf868f4570420a0
karapazar <omer.savas@hotmail.com.tr>
2020-01-22 17:14:26 +0300

    Yeni Hafta

---
+++ 2019-2020/Ara Dönem/C#/fibonacci/ConsoleApp8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = new int[2];
+
+            for (int i = 0; i < dizi.Length; i++)
+                dizi[i] = 0;
+
+            dizi[1] = 1;
+
+            Console.Write("Dizinin kaç elemanini hesaplayalim: ");
+            string gecici = Console.ReadLine();
+            int sinir = Convert.ToInt32(gecici);
+
+            for (int i = 3; i <= sinir; i++)
+            {
+                int g = dizi[0] + dizi[1];
+                dizi[0] = dizi[1];
+                dizi[1] = g;
+            }
+
+            //int toplam = dizi[0] + dizi[1];
+
+            /*int toplam = 0;
+            for (int i = 0; i < dizi.Length; i++)
+                toplam += dizi[i];*/
+
+            int toplam = dizi.Sum();
+
+            Console.WriteLine("Dizinin " + sinir + ". elemani: "+toplam);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2019-2020/Ara Dönem/C#/switch/switch/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayi girin: ");
+            string gecici = Console.ReadLine();
+            int sayi = Convert.ToInt32(gecici);
+
+            if(sayi == 1)
+                Console.WriteLine("Bir");
+            else if(sayi == 2)
+                Console.WriteLine("İki");
+            else
+                Console.WriteLine("1 yada 2 değil");
+
+            switch (sayi)
+            {
+                case 1:
+                    Console.WriteLine("Bir");
+                    break;
+                case 2:
+                    Console.WriteLine("İki");
+                    break;
+                default:
+                    Console.WriteLine("1 yada 2 değil");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
