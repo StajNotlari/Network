0eab7ec278a29b3cf3097a8d4d88de5ce9dff853
mertcan01k <60130180+mertcan01k@users.noreply.github.com>
2020-01-23 11:42:48 +0300

    #Kare alma

---
+++ 2020/Ara Dönem/C#/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulamalar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Karesi Alınacak Sayiyi Gir:" );
+            string a = Console.ReadLine();
+            int sayi = Convert.ToInt32(a);
+            Console.WriteLine(sayi * sayi);
+            Console.ReadKey();
+
+
+
+
+  
+     
+
+        }
+    }
+}
