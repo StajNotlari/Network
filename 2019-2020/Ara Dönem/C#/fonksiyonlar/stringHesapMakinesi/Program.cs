df1fe044392454adf1ded0863471555c8811d191
karapazar <omer.savas@hotmail.com.tr>
2020-01-27 12:40:20 +0300

    YeniÖrnekler

---
+++ 2019-2020/Ara Dönem/C#/fonksiyonlar/stringHesapMakinesi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringHesapMakinesi
+{
+    class Program
+    {
+        static void EkranaVerilenDatayiYaz(string data)
+        {
+            Console.WriteLine(data);
+        }
+
+        static void EkranaOmersavasYaz()
+        {
+            EkranaVerilenDatayiYaz("omersavas");
+        }
+
+
+        static int topla(int a, int b)
+        {
+            int toplam = a + b;
+            return toplam;
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+                EkranaOmersavasYaz();
+
+
+            int sonuc = topla(5, 6);
+            EkranaVerilenDatayiYaz(sonuc.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2019-2020/Ara Dönem/C#/stringHesapMakinesi/stringHesapMakinesi/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringHesapMakinesi
+{
+    class Program
+    {
+        static int[] sayilar = new int[20];
+        static string[] isaretler = new string[19];
+
+        static int isaretlerIndex, sayilarIndex;
+
+
+
+        static void DenklemiDizilereDoldur(string denklem)
+        {
+            isaretlerIndex = 0;
+            sayilarIndex = 0;
+
+            foreach (var tekKarakter in denklem)
+            {
+                int gecici;
+                bool sayiMi = int.TryParse(tekKarakter.ToString(), out gecici);
+
+                if (sayiMi)
+                    sayilar[sayilarIndex++] = gecici;
+                else
+                    isaretler[isaretlerIndex++] = tekKarakter.ToString();
+            }
+        }
+
+        static double DizilerdekiDatadanSonucGetir()
+        {
+            return 0.0;
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.Write("Denklem giriniz: ");
+            //string denklem = Console.ReadLine();
+
+            string denklem = "7/8+9+5";
+            DenklemiDizilereDoldur(denklem);
+            double sonuc = DizilerdekiDatadanSonucGetir();
+            Console.WriteLine("Sonuc: " + sonuc);
+
+            Console.ReadKey();
+        }
+    }
+}
