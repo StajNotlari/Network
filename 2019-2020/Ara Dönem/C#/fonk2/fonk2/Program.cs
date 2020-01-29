f94d601ade803493244143d782beb4f29907f9b9
Araz Babayev <56319506+Arazturksoy@users.noreply.github.com>
2020-01-29 11:50:44 +0300

    #FonksiyonOdev

---
+++ 2019-2020/Ara Dönem/C#/fonk2/fonk2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fonk2
+{
+    class Program
+    {
+        static void EkranaVerilenDatayiYaz(string data)
+
+        {
+            Console.WriteLine(data);
+        }
+    
+        static void EkranaArazbabayevYaz()
+
+        {
+            EkranaVerilenDatayiYaz("araz babayev");
+        }
+
+        static int Topla(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main()
+        {
+            int a = Topla(2, 5);
+            Console.Write(a);
+            Console.ReadKey();
+        }
+    }
+  }
+
